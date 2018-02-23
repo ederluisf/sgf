8f88f9981b87575eb4b3d313ef1c084fa6bfb789
Eder Fonseca <ederluisf@hotmail.com>
2018-02-23 05:58:04 -0300

    Tornando os repositories como transactional

--- sgf-api/src/main/java/br/com/sgf/api/repositories/UserRepository.java
+++ sgf-api/src/main/java/br/com/sgf/api/repositories/UserRepository.java
@@ -1,9 +1,11 @@
 package br.com.sgf.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.sgf.api.entities.User;
 
+@Transactional
 public interface UserRepository extends JpaRepository<User, Long> {
 	
 }
--- sgf-api/src/main/java/br/com/sgf/api/repositories/VehicleRepository.java
+++ sgf-api/src/main/java/br/com/sgf/api/repositories/VehicleRepository.java
@@ -1,9 +1,11 @@
 package br.com.sgf.api.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.sgf.api.entities.Vehicle;
 
+@Transactional
 public interface VehicleRepository extends JpaRepository<Vehicle, Long> {
 	
 }
